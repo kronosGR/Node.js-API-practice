5e88a4dea2014179e274d1dee7aeb77c78ce85c2
KronosGR <geo.elgeo@gmail.com>
2021-11-10 10:17:53 +0100

    change socket.io setup to be share to other files

--- app.js
+++ app.js
@@ -63,9 +63,7 @@ mongoose
   .connect('mongodb+srv://kronos:yxhI2XOMH63PzHrm@cluster0.hrnez.mongodb.net/messages')
   .then((result) => {
     const server = app.listen(8080);
-    const io = require('socket.io')(server, {
-      cors: 'localhost:3000'
-    });
+    const io = require('./socket').init(server);
     io.on('connection', socket => {
       console.log('Client connected')
     })
