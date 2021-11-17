c74246880896f2a51bdcf02367da285a687345c7
KronosGR <geo.elgeo@gmail.com>
2021-11-17 09:25:30 +0100

    more tests :)

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -20,4 +20,23 @@ describe('Auth middleware', function () {
     };
     expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
   });
+
+  it('should throw an error if token cannot be verified', function () {
+    const req = {
+      get: function (headerName) {
+        return 'Bearer xyz';
+      },
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
+
+  it('should yield a userid after decoding the token', function () {
+    const req = {
+      get: function (headerName) {
+        return 'Bearer xyz';
+      },
+    };
+    authMiddleware(req, {}, () => {});
+    expect(req).to.have.property('userId');
+  });
 });
