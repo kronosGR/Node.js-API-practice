04cfe0b8afd43d72f5d3f35a0eeeeda0ad2903fe
KronosGR <geo.elgeo@gmail.com>
2021-11-17 09:20:05 +0100

    multiple tests

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -2,11 +2,22 @@ const expect = require('chai').expect;
 
 const authMiddleware = require('../middleware/is-auth');
 
-it('should throw an error if no authorization header is presernt', function () {
-  const req = {
-    get: function (headerName) {
-      return null;
-    },
-  };
-  expect(authMiddleware.bind(this, req, {}, () => {})).to.throw('not authenticated')
+describe('Auth middleware', function () {
+  it('should throw an error if no authorization header is presernt', function () {
+    const req = {
+      get: function (headerName) {
+        return null;
+      },
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw('not authenticated');
+  });
+
+  it('should throw an error if the auth header is only one string', function () {
+    const req = {
+      get: function (headerName) {
+        return 'xyz';
+      },
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
 });
