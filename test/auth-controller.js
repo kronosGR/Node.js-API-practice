6ef7aab139f8d813fc04a0904c03ec422b8c84f7
kronosGR <geo.elgeo@gmail.com>
2021-11-18 08:29:43 +0100

    Async test with mocha

--- test/auth-controller.js
+++ test/auth-controller.js
@@ -1,17 +1,26 @@
 const expect = require('chai').expect;
 const sinon = require('sinon');
 
-const User = require('../models/user')
+const User = require('../models/user');
 const AuthController = require('../controllers/auth');
 
-describe('Auth Controller - Login', function() {
-  it('Shoud throw an error if db access fails', function() {
+describe('Auth Controller - Login', function () {
+  it('Shoud throw an error if db access fails', function (done) {
     sinon.stub(User, 'findOne');
     User.findOne.throws();
 
-    
+    const req = {
+      body: {
+        email: 'test@11.com',
+        password: '11111',
+      },
+    };
+    AuthController.login(req, {}, () => {}).then((result) => {
+      expect(result).to.be.an('error');
+      expect(result).to.have.property('statusCode', 500);
+      done();
+    });
 
     User.findOne.restore();
-
-  })
-})
+  });
+});
